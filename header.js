990ee7162dd42966d0b6631e511252f4d5f877df
Asger Jark Kappel <au606583@uni.au.dk>
2024-01-25 18:48:31 +0100

    Functionallity of dropdown restored, able to query the shadow tree (attributes)

--- header.js
+++ header.js
@@ -1,4 +1,6 @@
 //This renders the navbar on each page. Hidden in the shadow dom for encapsulation
+const host = document.querySelector("#header");
+
 const template = document.createElement("template");
 template.innerHTML = `
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -17,8 +19,25 @@ template.innerHTML = `
 class Test extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: "open" });
+    const host = document.querySelector("#header");
+    const shadow = host.attachShadow({ mode: "open" });
     shadow.append(template.content.cloneNode(true));
   }
 }
 customElements.define("nav-element", Test);
+
+function myFunction() {
+  console.log("Button pressed");
+  //You have to know the host-node (header element, now with ID) of the shadow root (the template) to query the element.
+  const host = document.getElementById("header");
+  console.log("this header " + host); //Debug
+  const child = host.shadowRoot;
+  console.log("child is " + child); //Debug
+  var x = child.querySelector(".topnav"); //Select first element with that attribute (We only have one topnav)
+  console.log(x.className); //Debug
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
