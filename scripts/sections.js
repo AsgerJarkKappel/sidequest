002baf1e15615c9a2a2684414d0b51dcb1a2dfa8
Asger Jark Kappel <au606583@uni.au.dk>
2024-02-21 16:38:13 +0100

    trying anchor tag with type attribute

--- scripts/sections.js
+++ scripts/sections.js
@@ -76,7 +76,7 @@ const aboutSection = `
 
             <div class="cv-btn-container">
                 <a href="#nested" class="cv-nav-btn">See Online CV</a> 
-                <a href="asgerjarkkappel.github.io/sidequestgithub/Resources/asger-cv.pdf" target="_blank" class="cv-nav-btn">Download CV PDF</a>
+                <a href="Resources/asger-cv.pdf" type="application/pdf" target="_blank" class="cv-nav-btn">Download CV PDF</a>
             </div>
         </div>
 
